d2be35d4a161f152cadf20fb0879600a48d98d9f
Darren Pham <phamdarren1@gmail.com>
2024-01-11 20:03:04 -0800

    upgarde argocd helm

--- aws-eks-cluster/argocd.tf
+++ aws-eks-cluster/argocd.tf
@@ -34,6 +34,7 @@ resource "helm_release" "argocd" {
   name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
+  version    = "5.52.0"
 
   # Utilize Cloudflare Tunnel
   set {
